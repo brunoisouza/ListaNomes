0e7b72b4b4984d31e6615f1b97cc7df8733641fd
brunoisouza <lubru70@hotmail.com>
2024-01-30 19:25:16 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaNomes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+            int[] idades = new int[10];
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine($"Informe o nome do {i + 1}º aluno");
+                nomes[i] = Console.ReadLine();
+
+                Console.WriteLine($"Informe a idade do {i + 1}º aluno");
+                idades[i] = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+            }
+
+            for (int index = 0; index < nomes.Length; index++)
+            {
+                Console.WriteLine($"nome: {nomes[index]} idade: {idades[index]}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
